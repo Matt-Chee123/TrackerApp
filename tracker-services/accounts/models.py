c52fba732979cdd8dd195dc2a62adf07e8ea22e9
Matt-Chee123 <cheematthew10@gmail.com>
2025-07-03 22:02:31 +0100

    sort out settings and models

--- tracker-services/accounts/models.py
+++ tracker-services/accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Account:
+    account_name = models.CharField(max_length=30)
+    user_id = models.ForeignKey('user.User',on_delete=models.CASCADE())
---
+++ tracker-services/user/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class User:
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
