65a65c8deb6413ebf9bf330898783ae7697ece2e
Matt-Chee123 <cheematthew10@gmail.com>
2025-07-05 18:39:19 +0100

    alter table

--- tracker-services/accounts/models.py
+++ tracker-services/accounts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
-class Account:
+class Account(models.Model):
     account_name = models.CharField(max_length=30)
-    user_id = models.ForeignKey('user.User',on_delete=models.CASCADE())
+    user_id  = models.ForeignKey('user.User',on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "user_account"
--- tracker-services/user/models.py
+++ tracker-services/user/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-class User:
+class User(models.Model):
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+    test = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "user_profile"
