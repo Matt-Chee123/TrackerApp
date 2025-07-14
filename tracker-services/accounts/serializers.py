081f31a82455d5b252b8b4e7f941c559a82ea31c
Matt-Chee123 <cheematthew10@gmail.com>
2025-07-14 21:37:13 +0100

    create holding serializer

--- tracker-services/accounts/serializers.py
+++ tracker-services/accounts/serializers.py
@@ -40,6 +40,32 @@ class TransactionsSerializer(serializers.ModelSerializer):
             return (obj.quantity * obj.price) + obj.fees
 
 
+class HoldingsSerializer(serializers.ModelSerializer):
+    current_value = serializers.SerializerMethodField()
+    total_cost = serializers.SerializerMethodField()
+    unrealized_pnl = serializers.SerializerMethodField()
 
+    class Meta:
+        model = Holdings
+        fields = [
+            'id', 'name', 'code', 'state', 'quantity', 'current_price',
+            'created_date', 'last_updated',
+            'current_value', 'total_cost', 'unrealized_pnl'
+        ]
+        read_only_fields = ['created_date', 'last_updated']
 
+        def get_current_value(self, obj):
+            return (obj.current_price * obj.quantity)
+
+        def get_total_cost(self, obj):
+            buy_transactions = obj.transactions_set.filter(transaction_type='buy')
+            cost = 0
+            for tx in buy_transactions:
+                cost += (tx.quantity * tx.price) + tx.fees
+            return cost
+
+        def get_unrealized_pnl(self, obj):
+            current_value = self.get_current_value(obj)
+            total_cost = self.get_total_cost(obj)
+            return current_value - total_cost
 
