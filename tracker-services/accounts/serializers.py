244f041808df252d6cb5414f979d721528211c47
Matt-Chee123 <cheematthew10@gmail.com>
2025-07-14 21:31:30 +0100

    add transaction cerial

--- tracker-services/accounts/serializers.py
+++ tracker-services/accounts/serializers.py
@@ -23,3 +23,23 @@ class AccountSerializer(serializers.ModelSerializer):
 
         def get_daily_pnl(self, obj): #TODO Do this one
             return
+
+class TransactionsSerializer(serializers.ModelSerializer):
+    total_account_value = serializers.SerializerMethodField()
+    transaction_type_display = serializers.CharField(source='get_transaction_type_display', read_only=True)
+
+    class Meta:
+        model = Transactions
+        fields = [
+            'id', 'transaction_type', 'transaction_type_display', 'quantity',
+            'price', 'fees', 'transaction_date', 'created_date',
+            'total_value'
+        ]
+
+        def get_total_value(self, obj):
+            return (obj.quantity * obj.price) + obj.fees
+
+
+
+
+
