4dd16bcdae3a77bfed0e0da45654b1d9a5a3f107
Matt-Chee123 <cheematthew10@gmail.com>
2025-07-16 21:18:58 +0100

    added views

--- tracker-services/accounts/serializers.py
+++ tracker-services/accounts/serializers.py
@@ -12,20 +12,20 @@ class AccountSerializer(serializers.ModelSerializer):
             'id','account_name','user','total_value','total_holdings','daily_pnl'
         ]
 
-        def get_total_value(self, obj):
-            total = 0
-            for holding in obj.holding_set.all():
-                total += (holding.current_price * holding.quantity)
-            return total
+    def get_total_value(self, obj):
+        total = 0
+        for holding in obj.holding_set.all():
+            total += (holding.current_price * holding.quantity)
+        return total
 
-        def get_total_holdings(self, obj):
-            return obj.holding_set.count()
+    def get_total_holdings(self, obj):
+        return obj.holding_set.count()
 
-        def get_daily_pnl(self, obj): #TODO Do this one
-            return
+    def get_daily_pnl(self, obj): #TODO Do this one
+        return
 
 class TransactionsSerializer(serializers.ModelSerializer):
-    total_account_value = serializers.SerializerMethodField()
+    total_value = serializers.SerializerMethodField()
     transaction_type_display = serializers.CharField(source='get_transaction_type_display', read_only=True)
 
     class Meta:
@@ -36,8 +36,8 @@ class TransactionsSerializer(serializers.ModelSerializer):
             'total_value'
         ]
 
-        def get_total_value(self, obj):
-            return (obj.quantity * obj.price) + obj.fees
+    def get_total_value(self, obj):
+        return (obj.quantity * obj.price) + obj.fees
 
 
 class HoldingsSerializer(serializers.ModelSerializer):
@@ -54,18 +54,18 @@ class HoldingsSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['created_date', 'last_updated']
 
-        def get_current_value(self, obj):
-            return (obj.current_price * obj.quantity)
+    def get_current_value(self, obj):
+        return (obj.current_price * obj.quantity)
 
-        def get_total_cost(self, obj):
-            buy_transactions = obj.transactions_set.filter(transaction_type='buy')
-            cost = 0
-            for tx in buy_transactions:
-                cost += (tx.quantity * tx.price) + tx.fees
-            return cost
+    def get_total_cost(self, obj):
+        buy_transactions = obj.transactions_set.filter(transaction_type='buy')
+        cost = 0
+        for tx in buy_transactions:
+            cost += (tx.quantity * tx.price) + tx.fees
+        return cost
 
-        def get_unrealized_pnl(self, obj):
-            current_value = self.get_current_value(obj)
-            total_cost = self.get_total_cost(obj)
-            return current_value - total_cost
+    def get_unrealized_pnl(self, obj):
+        current_value = self.get_current_value(obj)
+        total_cost = self.get_total_cost(obj)
+        return current_value - total_cost
 
--- tracker-services/accounts/views.py
+++ tracker-services/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Account, Holdings, Transactions
+from .serializers import AccountSerializer, HoldingsSerializer, TransactionsSerializer
 
-# Create your views here.
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user','account_name']
+    ordering_fields = ['created_date']
+    ordering = ['-created_date']
+
+class HoldingsViewSet(viewsets.ModelViewSet):
+    queryset = Holdings.objects.all()
+    serializer_class = HoldingsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['account']
+
+class TransactionsViewSet(viewsets.ModelViewSet):
+    queryset = Transactions.objects.all()
+    serializer_class = TransactionsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['account', 'transaction_type']
+
+
